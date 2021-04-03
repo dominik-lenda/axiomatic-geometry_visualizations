401ecc7c65385446a731f09d9ffa2295702f24c8
Dominik Lenda <dlenda1729@gmail.com>
2021-04-03 21:41:40 +0200

    add last connection axioms

--- src/Main.elm
+++ src/Main.elm
@@ -23,6 +23,24 @@ view model =
         , p []
             [ text "Two distinct points A and B always completely determine a straight line a. We write AB = a or BA = a."
             ]
+        , p []
+            [ text "Any two distinct points of a straight line completely determine that line; that is, if AB = a and AC = a, where B 6 = C, then is also BC = a."
+            ]
+        , p []
+            [ text "Three points A, B, C not situated in the same straight line always completely determine a\nplane α. We write ABC = a."
+            ]
+        , p []
+            [ text "Any three points A, B, C of a plane α, which do not lie in the same straight line, completely\ndetermine that plane."
+            ]
+        , p []
+            [ text "If two points A, B of a straight line a lie in a plane α, then every point of a lies in α."
+            ]
+        , p []
+            [ text "If two planes α, β have a point A in common, then they have at least a second point B in\ncommon."
+            ]
+        , p []
+            [ text "Upon every straight line there exist at least two points, in every plane at least three points\nnot lying in the same straight line, and in space there exist at least four points not lying\nin a plane."
+            ]
         ]
 
 
